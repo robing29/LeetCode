353ea04508e4d38208726812598bc841dcb1b446
Robin Gude <guderobin@gmail.com>
2023-08-04 23:13:45 +0200

    First solution apparently works, but "Time Limit Exceeded". Going to look up better solution tomorrow.

---
+++ Problem121/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem121
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i+1; j < prices.Length; j++)
+                {
+                    int temp = prices[j] - prices[i]; //Szenario für einen Verkauf an dem Tag
+                    maxProfit = temp > maxProfit ? temp : maxProfit; //Tag an dem man verkauft - Tag an dem man verkauft
+                }
+            }
+            return maxProfit;
+
+
+            //Sicherstellen, dass Profit wirklich positiv ist
+            //Buy before sell
+
+        }
+    }
+}
